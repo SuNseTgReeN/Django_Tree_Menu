d36f746137c54fe80b017623bb37a0a0e7302b9b
SuNseTgReeN <ahtohio_pal09@bk.ru>
2023-10-16 16:21:23 +0200

    add django tags

--- Tree_Menu/menu/templatetags/menu_tags.py
+++ Tree_Menu/menu/templatetags/menu_tags.py
@@ -3,20 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
-def draw_menu(menu):
-    return _render_menu(menu)
-
-
-def _render_menu(menu):
-    if not menu:
-        return ''
-
-    menu_html = '<ul>'
-    for item in menu.menu_set.all():
-        menu_html += '<li>'
-        menu_html += f'<a href="{item.url}">{item.name}</a>'
-        menu_html += _render_menu(item)
-        menu_html += '</li>'
-    menu_html += '</ul>'
-    return menu_html
+@register.inclusion_tag('menu/menu_list_tag.html')
+def render_menu(menu):
+    return {'menu': menu}
